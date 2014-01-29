3da1a064a2d2a1536dd058d145cf86035f770261
Chloe Eghtebas <nceghtebas@hotmail.com>
2014-01-28 20:10:13 -0500

    Refactored code in at least 3 functions without dealing with passing char array args. Code runs without any warnings with all the flags.

--- hw01/cards.c
+++ hw01/cards.c
@@ -7,13 +7,35 @@
  #include <stdio.h>
  #include <stdlib.h>
 
+/*First fucntion out of three
+ *Checks to see if val is within bounds*/
+int ValueChecker(int val, int count){
+		/*Check uf the value is 3 to 6*/
+	if (val >2 && val<7){
+		count++;
+	}else if(val ==10){ 
+		count--;
+	}
+	return val;
+}
+
+/*Second functin out of three
+ *Displays the current count*/
+void CurrentCount(int count){
+	printf("Current count: %i \n", count);
+}
+
+/*Third function  out of three because spent too 
+ *long dealing with passing in char arrays to 
+ *come up with more refactored functions
+ *good ol main function*/
 int main(){
 	char card_name[3];	
 	int count =0;
+	int val = 0;
 	while (card_name[0] != 'X'){
 		puts("Enter the card_name: ");
 		scanf("%2s", card_name);
-		int val =0;
 		switch (card_name[0]){
 		case 'K':
 		case 'Q':
@@ -33,17 +55,8 @@ int main(){
 			}
 		}
 	
-	//Check uf the value is 3 to 6
-	if ( (val >2) && (val<7) ){
-		count++;
-		//puts("Count has gone up");
-	}else if(val ==10){ 
-		count--;
-		//puts("Count has gone down");
-	}
-
-	printf("Current count: %i \n", count);
-	//printf("The card value is: %i \n", val);
+	count= ValueChecker(val, count);
+	CurrentCount(count);
 	}
 	return 0;
 	
