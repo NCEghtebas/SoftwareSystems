3806673d6b15afa9c37ba409ae6f5d98cab80159
Chloe Eghtebas <nceghtebas@hotmail.com>
2014-02-01 17:01:02 -0500

    Count now returns correct value. Renamed functions and refactored code into two additional functions for a total of 5.

--- hw01/cards.c
+++ hw01/cards.c
@@ -7,57 +7,67 @@
  #include <stdio.h>
  #include <stdlib.h>
 
-/*First fucntion out of three
+
+/*First fucntion out of five
+ *returns val based on user input*/
+int get_value(int val ,char card_name[]){
+	switch (card_name[0]){
+	case 'K':
+	case 'Q':
+	case 'J':	
+		val=10;
+		break;
+	case 'A':
+		val =11;
+		break;
+	case 'X':
+		return 0;
+	default:
+		val = atoi(card_name);
+		if ((val < 1) || (val>10) ){
+			puts("I don't understand that value!");
+		}
+	}
+	return val;
+}
+
+
+/*Second fucntion out of five
  *Checks to see if val is within bounds*/
-int ValueChecker(int val, int count){
+int value_checker(int val, int count){
 		/*Check uf the value is 3 to 6*/
 	if (val >2 && val<7){
 		count++;
 	}else if(val ==10){ 
 		count--;
 	}
-	return val;
+	return count;
 }
 
-/*Second functin out of three
+/*Third functin out of five
  *Displays the current count*/
-void CurrentCount(int count){
+void current_count(int count){
 	printf("Current count: %i \n", count);
 }
 
-/*Third function  out of three because spent too 
- *long dealing with passing in char arrays to 
- *come up with more refactored functions
+/*Fourth function out of five
+ *Evaluates face values of card */
+void black_jack(int val, char card_name[], int count){
+	while (card_name[0] != 'X'){
+		puts("Enter the card_name: ");
+		scanf("%2s", card_name);
+		val= get_value(val, card_name);
+		count= value_checker(val, count);
+		current_count(count);
+	}
+}
+
+/*Fifth function out of five 
  *good ol main function*/
 int main(){
 	char card_name[3];	
 	int count =0;
 	int val = 0;
-	while (card_name[0] != 'X'){
-		puts("Enter the card_name: ");
-		scanf("%2s", card_name);
-		switch (card_name[0]){
-		case 'K':
-		case 'Q':
-		case 'J':	
-			val=10;
-			break;
-		case 'A':
-			val =11;
-			break;
-		case 'X':
-			continue;
-		default:
-			val = atoi(card_name);
-			if ((val < 1) || (val>10) ){
-				puts("I don't understand that value!");
-				continue;
-			}
-		}
-	
-	count= ValueChecker(val, count);
-	CurrentCount(count);
-	}
+	black_jack(val, card_name, count);
 	return 0;
-	
 }
