e32aff798c40e553141540d96b7b0c8f2c66f1bf
NCEghtebas <nceghtebas@hotmail.com>
2014-02-20 12:14:38 -0500

    fixed bugs in code

--- exam1/regex.c
+++ exam1/regex.c
@@ -27,7 +27,8 @@ char tracks[][80] = {
 void find_track(char *search_for)
 {
     int i;
-    for (i=0; i<NUM_TRACKS; i++) {
+    for (i=0; i<5; i++)
+    {
 	if (strstr(tracks[i], search_for)) {
 	    printf("Track %i: '%s'\n", i, tracks[i]);
 	}
@@ -42,15 +43,16 @@ void find_track_regex(char *pattern)
 {
     int i, ret;
     regex_t regex;
-    char *msgbuf;
+    char msgbuf[100];
 
-    ret = regcomp(&regex, pattern, REG_EXTENDED || REG_NOSUB);
+    ret = regcomp(&regex, pattern, REG_EXTENDED || REG_NOSUB);//?
     if (ret) {
         fprintf(stderr, "Could not compile regex\n");
         exit(1);
     }
 
-    for (i=0; i<NUM_TRACKS; i++) {
+    for (i=0; i<5; i++)
+    {
         ret = regexec(&regex, tracks[i], 0, NULL, 0);
 	if (!ret) {
 	    printf("Track %i: '%s'\n", i, tracks[i]);
@@ -66,13 +68,13 @@ void find_track_regex(char *pattern)
     /* I'm not sure this is necessary, but it's possible that if you
        let regex go out of scope without running regfree, it leaks
        (that is, leaves some allocated memory unfreed). */
-    regfree(regex);
+    regfree(&regex);
 }
 
 
 int main (int argc, char *argv[])
 {
-    char *target = 'F';
+    char *target = "F";
     char *pattern = "Fr.*Fr.*";
 
     find_track(target);
